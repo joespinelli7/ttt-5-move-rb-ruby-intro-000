5af9e848acbea73bc68d17d24dcb05a6886c595b
Joseph M Spinelli <josephspinelli@yahoo.com>
2018-08-12 15:40:24 +0000

    automatically backed up by learn

--- lib/move2.rb
+++ lib/move2.rb
@@ -6,9 +6,9 @@ def display_board(board)
   puts "#{board[6]} | #{board[7]} | #{board[8]}"
 end
 
-def input_to_index(user_input)
-  new_user_input = user_input.to_i
-  new_user_input -= 1
+def input_to_index(input)
+  new_user_input = input.to_i #makes so input of user corrects to integer. So 2.4 = 2 and goes to space 2 on board.
+  new_user_input -= 1 #Subtract AND assignment operator, subtracts right operand from the left operand and assign the result to left operand.
   return new_user_input
 end
 
